defd16ef2a96be7e36aca0f41881861abfd34a70
moh2hrt <muhammadtorhir@gmail.com>
2022-09-08 19:32:36 -0700

    updated task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
 *main - Print my style
 *
-*Return: Always o (successful)
+*Return: Always 0 (successful)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
