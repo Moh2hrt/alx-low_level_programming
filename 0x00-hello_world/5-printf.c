c54c6fac30aed98f48948f0a9b10b613cee7258f
moh2hrt <muhammadtorhir@gmail.com>
2022-09-08 19:25:05 -0700

    on God

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Print my style
- *
- *Return: Always o (successful)
- */
+*main - Print my style
+*
+*Return: Always o (successful)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
