f31596dccc39a2498e9a0ca034e6e3b36d9783e3
moh2hrt <muhammadtorhir@gmail.com>
2022-09-09 16:43:32 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
 * main - Entry point
+*
+*
 *
 *Return: 0
 */
 
 int main(void)
 {
-int d;
-for (d = '0'; d <= '9'; d++)
+int n1 = 48;
+int n2;
+int com = 44;
+(n1 <= 57)
 {
-putchar(d);
-(d 1 = '9')
+n2 = n1 + 1;
+(n2 <= 57)
 {
-putchar(',');
-putchar(' ');
+putchar(n1);
+putchar(n2);
+(n1 1 = 56 | | n2 1 = 57)
+{
+putchar(com);
+putchar(32);
 }
+n2 + -1;
+}
+n1 + -1;
 }
 putchar('\n');
 return (0);
 }
+
