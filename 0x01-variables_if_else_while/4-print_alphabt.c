1aa5af8908fba68d493bd517efa42b4d4278948b
moh2hrt <muhammadtorhir@gmail.com>
2022-09-09 15:28:29 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: 0
+*/
+
+int main(void)
+{
+int lw = 'a';
+while (lw <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+lw += 1;
+}
+else
+{
+putchar(lw);
+lw += 1;
+}
+}
+putchar('\n');
+return (0);
+}
