b8d20c933bfad346cb0dbf24fc1611c786928aa6
moh2hrt <muhammadtorhir@gmail.com>
2022-09-13 09:38:32 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
+#ifindef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
-
-/**
-* main - entry point
-*
-*return: alswyas 0
-*
-*/
 
 int _putchar(char);
 void print_alphabet(void)
@@ -18,11 +12,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
-void print_to_98
-(int n);
-void print_times_table
-(int n);
+void print_to_98(int n);
+void print_times_table(int n);
 void num(int r, int n);
-
 
 #endif
