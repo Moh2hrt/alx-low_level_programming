cb659446898c2d0d11ea88099cdac00462b31647
moh2hrt <muhammadtorhir@gmail.com>
2022-09-13 10:26:34 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h" 
-  
- /** 
- * main - print putchar
- * Return: always 0 
- */ 
- int main(void) 
- { 
-         _putchar('_'); 
-         _putchar('p'); 
-         _putchar('u'); 
-         _putchar('t'); 
-         _putchar('c'); 
-         _putchar('h'); 
-         _putchar('a'); 
-         _putchar('r'); 
-         _putchar('\n'); 
-  
-         return (0); 
- }
+#include "main.h"
+
+/**
+*main - print putchar
+*Return: always 0
+*/
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
