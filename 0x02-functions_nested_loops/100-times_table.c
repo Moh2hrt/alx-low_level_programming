dffdcf3d4374cff2a514bc5f2316b72e7ee75b5b
moh2hrt <muhammadtorhir@gmail.com>
2022-09-14 09:41:20 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-* print_times_table - prints time table
-* @n : use in times table
-* Return:void
+*print times_table - prints time table
+*@n : use in times table
+*Return:void
 */
 
-void print_times_table(int n)
-
+void print times_table(int n)
 {
 int a = 0, rep, b;
 
