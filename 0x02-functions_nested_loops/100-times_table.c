e966cb15289f8712ea34c88c09d64acf5236ebf1
moh2hrt <muhammadtorhir@gmail.com>
2022-09-14 12:47:28 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,50 @@
 #include "main.h"
 
 /**
-*print_times_table - prints time table
-*@n : use in times table
-*Return: void
-*/
+ *print_times_table - prints time table
+ *@n : use in times table
+ *Return:void
+ */
 
 void print_times_table(int n)
 {
+	int a = 0, rep, b;
 
-int a = 0, rep, b;
+	if (n < 0 || n > 15)
+		return;
 
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	while (a <= n)
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		for (b = 0; b <= n; b++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar (' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		else if (rep < 100)
-		{
-			_putchar (' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar('\n');
+		a++;
 	}
-	_putchar('\n');
-	a++;
-}
 }
