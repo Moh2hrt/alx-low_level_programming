4d812b58e324b0c9632f0ae9e44348988f221f37
moh2hrt <muhammadtorhir@gmail.com>
2022-09-14 01:12:14 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,99 +1,17 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef "main.h"
+#define "main.h"
 
 int _putchar(char c);
-/**
-*_putchar -writes the character c to std
-*@c: parameter to compare
-*Return: 1 or 0
-*/
-
-print_alphabet(void);
-/**
-*print_alphabet - function to print alphabet
-*
-*/
-
-print_alphabet_x10(void);
-/**
-*print_alphabet_x10 - function to print alphabet 10 times
-*
-*/
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
-/**
-*_islower - function to print c in lowercase
-*c is character to use
-*Return 1 or 0
-*/
-
 int _isalpha(int c);
-/**
-*_isalpha is a function to print if character is alphabet
-*c is character of use
-*Return 1 or 0
-*/
-
 int print_sign(int n);
-/**
- *print_sign - prints a sign based on value of n
- *n is integer input
- *Return: 1 or 0 or -1
- */
-
-int _abs(int r);
-/**
-*_abs - main funtion
-*
-*@r: is character to compare
-*
-*/
-
-int print_last_digit(int n);
-/**
-*print_last_digit -main function
-*
-*@n: is character
-*
-*/
-
-jack_bauer(void);
-/**
-*jack_baeur -main function
-*
-*Return: always 0
-*
-*/
-
-int add(int x, int y);
-/**
-*add- main function
-*
-*@x: first number
-*@y: second number
-*
-*/
-
-times_table(void);
-/**
-*times_table - main function
-*
-*/
-
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
-/**
-*print_to_98 - main function
-*
-*@n: character
-*
-*/
-
-void print_times_table(int n);
-/**
-*print_times_table -  main function
-*
-*@n: is character to compare
-*
-*/
 
 #endif
