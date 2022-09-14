7dc51436b9776977faf7d3f9816a6218166e4b6d
moh2hrt <muhammadtorhir@gmail.com>
2022-09-14 08:38:14 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,6 @@
 *Return: 0
 */
 int main(void)
-
 {
 
 int a = 0, b = 1, next = 0;
@@ -14,24 +13,16 @@ int a = 0, b = 1, next = 0;
 int sum = 0;
 
 while (next < 4000000)
-
-
 {
 
-	next = a + b;
-
-	a = b;
-
-	b = next;
+next = a + b;
+a = b;
+b = next;
 
 if (next % 2 == 0)
-
 sum += next;
-
 }
-
 printf("%i\n", sum);
-
 return (0);
 }
 
