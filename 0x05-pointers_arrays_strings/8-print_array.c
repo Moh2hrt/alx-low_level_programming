6983bce49c4022b8bdc6145acbc7c9a5a78d84f7
moh2hrt <muhammadtorhir@gmail.com>
2022-09-19 06:35:14 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,32 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * puts_half - print out half of the  string
- * @str: pointer argument
- * Description: returns the required result
- * Return: returns void
+ * print_array - print n element of an array integer
+ * @a: first argument
+ * @n: second argument
+ * Description: return the required result
+ *Return: void
  */
 
-void puts_half(char *str)
+void print_array(int *a, int n)
 {
-char *ptr = str;
-int count = 0;
-int j;
+	int *ptr = a, i;
 
-while (*ptr != '\0')
-{
-count++;
-ptr++;
-}
-
-for (j = 0; j <= (count - 1); j++)
-{
-if (j >= ((count) / 2) && j <= count && count % 2 == 0)
-{
-_putchar(str[j]);
-}
-else if (count % 2 != 0 && j > ((count - 1) / 2) && j <= count)
-_putchar(str[j]);
-}
-_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", ptr[i]);
+		else if (i > 0)
+		{
+			printf(", ");
+			printf("%d", ptr[i]);
+		}
+	}
+	printf("%c", '\n');
 }
