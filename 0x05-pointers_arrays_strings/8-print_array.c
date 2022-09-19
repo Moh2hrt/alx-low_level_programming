9293870e765fe9022505bcf4164894a455fde31f
moh2hrt <muhammadtorhir@gmail.com>
2022-09-19 06:20:01 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,24 +9,24 @@
 
 void puts_half(char *str)
 {
-	char *ptr = str;
-	int count = 0;
-	int j;
+char *ptr = str;
+int count = 0;
+int j;
 
-	while (*ptr != '\0')
-	{
-		count++;
-		ptr++;
-	}
+while (*ptr != '\0')
+{
+count++;
+ptr++;
+}
 
-	for (j = 0; j <= (count - 1); j++)
-	{
-		if (j >= ((count) / 2) && j <= count && count % 2 == 0)
-		{
-			_putchar(str[j]);
-		}
-		else if (count % 2 != 0 && j > ((count - 1) / 2) && j <= count)
-			_putchar(str[j]);
-	}
-	_putchar('\n');
+for (j = 0; j <= (count - 1); j++)
+{
+if (j >= ((count) / 2) && j <= count && count % 2 == 0)
+{
+_putchar(str[j]);
+}
+else if (count % 2 != 0 && j > ((count - 1) / 2) && j <= count)
+_putchar(str[j]);
+}
+_putchar('\n');
 }
